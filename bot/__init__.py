e4796529a40a5bc87dbcc230d658ffd04078da40
Akshay Chand <96618953+akshaychand08@users.noreply.github.com>
2025-06-27 19:43:13 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -129,5 +129,5 @@ TL_FILE_TYPES = (
 )
 
 AUTO_DELETE_TIME = int(get_config("AUTO_DELETE_TIME", 0))
-AUTH_GROUP = [int(i.strip()) for i in get_config("AUTH_GROUP", []).split(",")] if get_config("AUTH_GROUP", []) else []
+AUTH_GROUP = [int(i.strip()) for i in get_config("AUTH_GROUP", [-1001779363583]).split(",")] if get_config("AUTH_GROUP", [-1002575153636]) else []
 print(AUTH_GROUP)
